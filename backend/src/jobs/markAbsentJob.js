71bd53a16ca0055f9de62fa93899096a03f0c7fe
safeer <safeer997@gmail.com>
2025-05-01 01:09:34 +0530

    change cron job time

--- backend/src/jobs/markAbsentJob.js
+++ backend/src/jobs/markAbsentJob.js
@@ -2,9 +2,9 @@ import cron from 'node-cron';
 import { ClassSession } from '../models/classSession.model.js';
 import markAbsentStudents from '../services/update.attendence.js';
 
-// Cron job: runs every minute
-cron.schedule('* * * * *', async () => {
-  console.log(`[CRON] Job started at ${new Date().toLocaleString()}`);
+
+cron.schedule('0 * * * *', async () => {
+  // console.log(`[CRON] Job started at ${new Date().toLocaleString()}`);
   try {
     const now = new Date();
     const tenMinutesAgo = new Date(now.getTime() - 10 * 60 * 1000);
@@ -14,7 +14,9 @@ cron.schedule('* * * * *', async () => {
     });
 
     if (sessions.length === 0) {
-      console.log(`No sessions [CRON] Job found at ${new Date().toLocaleString()}`)
+      // console.log(
+      //   `No sessions [CRON] Job found at ${new Date().toLocaleString()}`
+      // );
       return;
     }
     for (const session of sessions) {
