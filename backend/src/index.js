0dfe65a02d1ba497f1533e7b3c84310bcff23d24
safeer <safeer997@gmail.com>
2025-05-02 00:24:27 +0530

    update cors setting

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,7 +26,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: 'http://localhost:5173', // your frontend origin
+    origin: 'https://marksafeer.onrender.com', // your frontend origin
     credentials: true, // allow cookies and credentials
   })
 );
